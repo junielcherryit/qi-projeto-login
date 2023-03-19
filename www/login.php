3164543e329cf961d1f6547a495103f0e5452617
Juniel Brizola <junnybrizola39@gmail.com>
2023-03-19 20:11:17 -0300

    ajuste

---
+++ www/login.php
@@ -0,0 +1,17 @@
+<?php
+    $email = $_POST['email'];
+    // $entrar = $_POST['entrar'];
+    $senha = md5($_POST['senha']);
+    $connect = mysqli_connect('db','root','password', 'qi');
+    if (isset($email)) {
+        $verifica = mysqli_query($connect, "SELECT * FROM user WHERE email =
+        '$email' AND senha = '$senha'") or die("erro ao selecionar");
+        if (mysqli_num_rows($verifica)<=0){
+            echo"<script language='javascript' type='text/javascript'>alert('E-mail e/ou senha incorretos');window.location.href='login.html';</script>";
+            // die();
+        } else{
+            setcookie("email",$email);
+            header("Location:index.php");
+        }
+    }
+?>
